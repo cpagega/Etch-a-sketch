cdce8ba1dfd27c286b7f45a793e40960276d0b87
Christopher Page <chrispage7@gmail.com>
2020-01-09 17:10:02 -0500

    Add variable grid size based on user input, mouseover add a fill class to divs to change color

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const container = document.querySelector('#container');
+const root = document.documentElement;
+const gridScale = 1.001;
+const maxGridSize = 200;
+const minGridSize = 2;
+let containerSize = 500;  // base container size - will need to adjust slightly based on number of divs
+let gridSize = prompt("Enter grid size between 2 and 200. eg. 4 for 4x4");
 
-for (let i = 0; i < 16; ++i)
+// bounds checking. rather than error, just set to min or max if bound exceeded.
+if (gridSize > maxGridSize)
+    gridSize = maxGridSize;
+if (gridSize < minGridSize)
+    gridSize = minGridSize;
+
+let squareSize = (containerSize / gridSize);
+containerSize = (squareSize * gridSize) * gridScale;   
+
+root.style.setProperty('--div-width', squareSize + "px");
+root.style.setProperty('--div-height', squareSize + "px");
+root.style.setProperty('--container-height', containerSize + "px");
+root.style.setProperty('--container-width', containerSize + "px");
+
+for (let i = 0; i < (gridSize*gridSize); ++i)
 {
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'div-format');
-    container.appendChild(newDiv);
+    let gridSquare = document.createElement('div');
+    container.appendChild(gridSquare);
+    gridSquare.addEventListener("mouseover", e=>{
+        gridSquare.classList.add('fill');
+    });
 }
