94e6298bf462dd171a22b207e19b2124c4b1bdbb
Christopher Page <chrispage7@gmail.com>
2020-01-09 17:18:09 -0500

    fixed containerSize math

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('#container');
 const root = document.documentElement;
-const gridScale = 1.001;
+const gridScale = 1.001;  // small scaler needed to fit grids, found .1% to work well.
 const maxGridSize = 200;
 const minGridSize = 2;
-let containerSize = 500;  // base container size - will need to adjust slightly based on number of divs
+let containerSize = 500;  
 let gridSize = prompt("Enter grid size between 2 and 200. eg. 4 for 4x4");
 
 // bounds checking. rather than error, just set to min or max if bound exceeded.
@@ -12,9 +12,10 @@ if (gridSize > maxGridSize)
 if (gridSize < minGridSize)
     gridSize = minGridSize;
 
-let squareSize = (containerSize / gridSize);
-containerSize = (squareSize * gridSize) * gridScale;   
+let squareSize = (containerSize / gridSize); 
+containerSize *= gridScale;
 
+// set CSS variables
 root.style.setProperty('--div-width', squareSize + "px");
 root.style.setProperty('--div-height', squareSize + "px");
 root.style.setProperty('--container-height', containerSize + "px");
